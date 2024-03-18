7dc8442d1e666e01559b9e0c3bf2560ed39da23a
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2024-03-18 23:09:10 +0000

    setup/create files

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from functools import wraps
+from flask import Flask, render_template, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_session import Session
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def hello_world():
