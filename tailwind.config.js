bba675506c1261136938f4bab8b8b64fb30c9e33
Selyss <99344963+Selyss@users.noreply.github.com>
2024-03-19 01:32:45 +0000

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.html"],
+  corePlugins: [
+    "fontSmoothing",
+    "fontFamily",
+    "fontSize",
+    "accessibility",
+    "flex",
+    "gap",
+    "margin",
+    "padding",
+  ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
     extend: {},
   },
   plugins: [],
