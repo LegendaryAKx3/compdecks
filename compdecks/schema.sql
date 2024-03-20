ff640db97f937a7012faed3701a966c2b9ba01de
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2024-03-20 23:08:48 +0000

    add schema for decks

--- compdecks/schema.sql
+++ compdecks/schema.sql
@@ -8,3 +8,10 @@ CREATE TABLE users (
     username TEXT NOT NULL,
     password TEXT NOT NULL
 );
+
+-- TODO store questions and answers for the deck in database
+CREATE TABLE decks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    owner_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+);
