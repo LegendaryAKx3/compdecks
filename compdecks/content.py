4a100ae2ca7ec35b5cbd3e82a37ac35b6c3feba9
Selyss <99344963+Selyss@users.noreply.github.com>
2024-03-21 22:08:28 +0000

    testing deck idea

--- compdecks/content.py
+++ compdecks/content.py
@@ -13,9 +13,32 @@ def index():
     # db = get_db()
     return render_template("content/index.html", title="Hello")
 
+
 @bp.route("/create", methods=["GET", "POST"])
 def create_deck():
     if request.method == "POST":
         ...
         # Figure out how csv file editing system will work
     return render_template("create.html")
+
+
+class Deck:
+    def __init__(self):
+        self.id = 1
+        self.name = "Good Deck"
+        self.owner = "Me 1"
+        self.questions = "10"
+        self.file_path = "/d//d/"
+
+
+@bp.route("/deck/<int:deck_id>", methods=["GET"])
+def deck_details(deck_id):
+    # get deck details from the database
+    ...
+
+    # get questions from the deck
+    ...
+
+    # TODO: REMOVE, THIS IS FOR TESTING PURPOSES
+    deck = Deck()
+    return render_template("content/deck_details.html", deck=deck)
